a5e4fd7fd5a506d4235377b018b730202fcba7ee
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-08-15 17:29:51 +0300

    1. Quartz [#175122]

---
+++ src/main/java/com/github/elizalex/grabber/quartz/AlertRabbit.java
@@ -0,0 +1,53 @@
+package com.github.elizalex.grabber.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.*;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+
+    private static Properties prop;
+
+    public static void initProperties(String propertiesFileName) {
+        prop = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream(propertiesFileName)) {
+            prop.load(in);
+        } catch (IOException ex) {
+            System.out.println("Ошибка чтения конфигурационного файла: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        initProperties("rabbit.properties");
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(Integer.parseInt(prop.getProperty("rabbit.interval")))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+}
